bf69bf58c1169673663de840b8448f1d1bd89621
chenghong <chenghong@chenghongtekimac-mini.local>
2017-10-16 14:23:47 +0800

    HT

--- HTLottery/Lottery/ViewController.m
+++ HTLottery/Lottery/ViewController.m
@@ -16,19 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // 大乐透的玩法：前区号码范围1-35，随机选取5个不重复的数；后区号码范围1-12，随机选取2个不重复的数，头奖即所有号码完全一致
+    // 假定当期头奖结果
+    NSArray *f = @[@(5), @(10), @(18), @(25), @(33)];  // 前区号码
+    NSArray *b = @[@(3), @(12)];  // 后区号码
     
-    NSArray *f = @[@(5), @(10), @(18), @(25), @(33)];
-    NSArray *b = @[@(3), @(12)];
-    
-    int r = 0;
+    int r = 0;  // 一致的号码个数，r == 7时代表全中
     NSMutableArray *frontResult = [[NSMutableArray alloc] init];
     NSMutableArray *backResult = [[NSMutableArray alloc] init];
-    int count = 0;
+    int count = 0;  // 需要购买的彩票注数
     
     do {        
         [frontResult removeAllObjects];
         [backResult removeAllObjects];
         
+        // 得出前区号码
         for (int i = 0; i < 5; i++) {
             int a;
             
@@ -39,6 +41,7 @@
             [frontResult addObject:@(a)];
         }
         
+        // 得出后区号码
         for (int i = 0; i < 2; i++) {
             int b;
             
